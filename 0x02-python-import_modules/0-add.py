879b1aefdf20a51f2ba4bb4b2a60ad5d10432201
foyem <anifowose.adeyemi@gmail.com>
2022-06-02 14:09:11 +0100

    new file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,7 +4,8 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b,  add(a, b)))
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
 if __name__ == "__main__":
     main()
